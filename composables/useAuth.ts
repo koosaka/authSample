659b2de1ee77e1292e9cbe82a9f15cb37ffe437f
koosaka <kosakatakumi.sys@gmail.com>
2023-02-19 14:11:47 +0900

    ログインページからユーザの登録動作が確認できた

---
+++ composables/useAuth.ts
@@ -0,0 +1,76 @@
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut as firebaseSignOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+export const useAuth = () => {
+  const token = useState<string | null>("token", () => null);
+
+  async function signIn(email: string, password: string) {
+    return await new Promise<void>((resolve, reject) => {
+      const auth = getAuth();
+      return signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          userCredential.user
+            .getIdToken()
+            .then((idToken) => {
+              token.value = idToken;
+              resolve();
+            })
+            .catch(reject);
+        })
+        .catch(reject);
+    });
+  }
+
+  async function signOut() {
+    return await new Promise<void>((resolve, reject) => {
+      const auth = getAuth();
+      firebaseSignOut(auth)
+        .then(() => {
+          token.value = null;
+          resolve();
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
+  }
+
+  async function checkAuthState() {
+    return await new Promise<void>((resolve, reject) => {
+      // client only
+      if (process.server) return resolve();
+      const auth = getAuth();
+      onAuthStateChanged(
+        auth,
+        (user) => {
+          if (user) {
+            user
+              .getIdToken()
+              .then((idtoken) => {
+                token.value = idtoken;
+                resolve();
+              })
+              .catch(reject);
+          } else {
+            token.value = null;
+            resolve();
+          }
+        },
+        (error) => {
+          reject(error);
+        }
+      );
+    });
+  }
+
+  return {
+    signIn,
+    signOut,
+    token,
+    checkAuthState,
+  };
+};
---
+++ pages/register/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+const user = reactive({
+  name: "",
+  email: "",
+  password: "",
+});
+
+const submit = async () => {
+  const auth = getAuth();
+  const response = await createUserWithEmailAndPassword(
+    auth,
+    user.email,
+    user.password
+  );
+};
+</script>
+
+<template>
+  <div>ここが登録ページ</div>
+  <input type="text" placeholder="ユーザネーム" v-model="user.name" />
+  <input type="email" placeholder="メールアドレス" v-model="user.email" />
+  <input type="password" placeholder="パスワード" v-model="user.password" />
+  <button type="submit" @click="submit()">送信</button>
+</template>
