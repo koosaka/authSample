c231178a69e0e0dac9d7cbd090ddafeaf3976450
koosaka <kosakatakumi.sys@gmail.com>
2023-02-19 14:45:18 +0900

    ログインページにログイン機能を追加

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,5 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+const { signIn } = useAuth();
+
+const user = reactive({
+  email: "",
+  password: "",
+});
+
+const submit = async () => {
+  signIn(user.email, user.password);
+};
+</script>
 
 <template>
   <div>ここがログインページ</div>
+  <input type="email" placeholder="メールアドレス" v-model="user.email" />
+  <input type="password" placeholder="パスワード" v-model="user.password" />
+  <button type="submit" @click="submit()">ログイン</button>
 </template>
