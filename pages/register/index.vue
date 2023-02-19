7c9de65cdbfcd39fbf07199ff4b20639e735c927
koosaka <kosakatakumi.sys@gmail.com>
2023-02-19 17:03:33 +0900

    profile_icon以外の項目をユーザ登録画面から選択できるように修正

--- pages/register/index.vue
+++ pages/register/index.vue
@@ -25,12 +25,32 @@ const registerUser = async () => {
   <div>はじめまして、まずはあなたの情報を教えてください。</div>
   <input type="text" placeholder="ユーザネーム" v-model="user.user_name" />
   <input type="email" placeholder="メールアドレス" v-model="auth.email" />
-  <input type="text" placeholder="誕生日" v-model="user.birthday" />
+  <div>
+    <input type="radio" id="man" v-model="user.gender" :value="0" checked />
+    <label for="mab">男性</label>
+  </div>
+
+  <div>
+    <input type="radio" id="woman" v-model="user.gender" :value="1" />
+    <label for="woman">女性</label>
+  </div>
+
+  <div>
+    <input type="radio" id="other" v-model="user.gender" :value="2" />
+    <label for="other">その他</label>
+  </div>
+  <input type="date" placeholder="誕生日" v-model="user.birthday" />
   <input
     type="text"
     placeholder="プロフィールアイコン"
     v-model="user.profile_icon"
   />
   <input type="password" placeholder="パスワード" v-model="auth.password" />
+  <div>
+    <a href="https://menherasenpai.notion.site">利用規約</a
+    >をご確認頂き、利用規約に同意するボタンをクリックしてください。
+  </div>
+  <input id="agreement" type="checkbox" v-model="user.is_agreement" />
+  <label for="agreement">利用規約に同意する</label>
   <button type="submit" @click="registerUser()">送信</button>
 </template>
